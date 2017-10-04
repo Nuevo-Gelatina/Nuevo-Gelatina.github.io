18803e5ce437b6326c3417f566f0c4ca2922c43c
Nuevo-Gelatina <31247859+Nuevo-Gelatina@users.noreply.github.com>
2017-10-04 23:37:15 +0100

    MEE
    meee

---
+++ _posts/2017-09-09-Test-test-tes.md
@@ -0,0 +1,12 @@
+---
+layout: post
+title:  "Test"
+date:   2014-12-28 15:29:54
+category: art
+---
+
+Test
+
+Test
+
+Test
---
+++ button.js
@@ -0,0 +1,17 @@
+function hover(object, no){
+    if(pageno==no)
+	return;
+	else{
+	object.style.backgroundColor="#cccccc";
+	    return;
+	}
+    }
+function unhover(object, no){
+    if(pageno==no){
+	return;
+    }
+    else{
+	object.style.backgroundColor="#ffffff";
+	return;
+    }
+}
